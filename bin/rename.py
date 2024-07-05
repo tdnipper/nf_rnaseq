79ef8af7c6fa0b5b59ba3f4a00114336d342836e
Thomas Nipper <nipper@wisc.edu>
2024-07-05 11:55:00 -0500

    accept sys.argv[1] as file to edit, not os.walk

--- bin/rename.py
+++ bin/rename.py
@@ -1,13 +1,20 @@
-import os
+#!/usr/bin/env python
 
-for dirpath, dirname, files in os.walk("raw_data"):
-    for file in files:
-        basename = os.path.basename(file)
-        parts = basename.split("_")
-        if len (parts) >= 4:
-            sample = parts[0]
-            pair = parts[3]
-            new_filename = f"{sample}_{pair}"
-            old_path = os.path.join(dirpath, file)
-            new_path = os.path.join(dirpath, new_filename)
-            os.rename(old_path, new_path)
+import os
+import sys
+
+infile = sys.argv[1]
+
+def rename_files(file):
+    basename = os.path.basename(file)
+    parts = basename.split("_")
+    if len(parts) >= 4:
+        sample = parts[0]
+        pair = parts[3]
+        new_filename = f"{sample}_{pair}.fastq.gz"
+        os.rename(file, new_filename)
+        # print(f"Renamed {file} to {new_filename}")
+
+
+
+rename_files(infile)
